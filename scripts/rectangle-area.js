dd8b30886438d66de25e5bb2cc8c4d496bf7cdb8
Programming Hero <programming.hero1@gmail.com>
2024-02-02 16:25:07 -0600

    calculated area of the rectangle

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea(){
-    console.log('calculate rect area');
+    // get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width)
+
+    // calculate rectangle area
+    const area = length * width;
+    console.log('area of the rectangle: ', area);
+
+    // display rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
